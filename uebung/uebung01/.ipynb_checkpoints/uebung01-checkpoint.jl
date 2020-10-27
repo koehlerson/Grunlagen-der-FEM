6e7cc9a0eee0dfecb8146b3ff909e627ad88da4c
ali mjalled <ali.mjalled1@gmail.com>
2020-10-27 11:48:28 +0100

    uebung1-done

---
+++ uebung/uebung01/.ipynb_checkpoints/uebung01-checkpoint.jl
@@ -0,0 +1,48 @@
+### A Pluto.jl notebook ###
+# v0.12.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 13693e5c-1465-11eb-0b9b-4b560ab73661
+md"""# Grundlagen der FEM
+Wilkommen in der ersten Übung im Modul Grundlagen der FEM. In der ersten Übung werden wir uns mit Tensoren und Kontinuumsmechanik
+beschäftigen.
+## Übung 1 Teil 1 - Tensorrechnung"""
+
+# ╔═╡ f95578fa-1464-11eb-06be-a3bc5c1d47e0
+html"""<iframe width="100%" height="450px" src="https://www.youtube.com/embed/W5UVb3BIbEw" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"""
+
+# ╔═╡ 8ad346fc-1791-11eb-0c61-63c29ca2c2df
+md"""
+## 1.1 Tensormultiplikation
+
+Schreiben Sie die folgenden Ausdrücke in Indexnotation und vereinfachen Sie soweit es geht.
+
+1. $\boldsymbol{I} : (\boldsymbol{A} \otimes \boldsymbol{b})$
+2.
+
+"""
+
+# ╔═╡ bc074770-1465-11eb-3f13-f30e20bf2ad6
+md"## Übung 1 Teil 2 - Kontinuumsmechanik"
+
+# ╔═╡ 053ff316-1465-11eb-180f-cbb42b439c66
+html"""<iframe width="100%" height="450px" src="https://www.youtube.com/embed/w1c7ezb3i2w" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"""
+
+# ╔═╡ 50bd15a0-146d-11eb-23a8-e535d61f3092
+md"""## Aufgabe 1.4
+
+"""
+
+# ╔═╡ 280a0fae-146e-11eb-2f2f-fdb24267944d
+HTML(open(f->read(f, String), "ue1_scaled.svg"))
+
+# ╔═╡ Cell order:
+# ╟─13693e5c-1465-11eb-0b9b-4b560ab73661
+# ╟─f95578fa-1464-11eb-06be-a3bc5c1d47e0
+# ╟─8ad346fc-1791-11eb-0c61-63c29ca2c2df
+# ╟─bc074770-1465-11eb-3f13-f30e20bf2ad6
+# ╟─053ff316-1465-11eb-180f-cbb42b439c66
+# ╠═50bd15a0-146d-11eb-23a8-e535d61f3092
+# ╟─280a0fae-146e-11eb-2f2f-fdb24267944d
